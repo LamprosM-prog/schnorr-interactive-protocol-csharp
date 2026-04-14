b41700ef2ef3fcbfbf94470ce4b817b2484de4aa
lol <papp12345r@gmail.com>
2026-04-14 22:15:05 +0300

    Add arithmetic

---
+++ src/Arithmetic/ModMath.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Numerics;
+
+
+namespace Arithmetic
+{
+    public class ModMath
+    {
+        public static BigInteger Add(BigInteger a, BigInteger b, BigInteger p) => (a + b) % p;
+        public static BigInteger Multiply(BigInteger a, BigInteger b, BigInteger p) => (a *b) % p;
+        public static BigInteger Pow(BigInteger a, BigInteger b, BigInteger p) => BigInteger.ModPow(a, b, p);
+    }
+}
